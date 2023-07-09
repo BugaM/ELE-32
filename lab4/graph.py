799c3fb93d32aa0273af916ed1c1df807e1c3214
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2023-07-09 11:28:13 +0200

    implement decode

--- lab4/graph.py
+++ lab4/graph.py
@@ -1,18 +1,17 @@
 from abc import ABC
-import csv
 import random
 from functools import reduce
 
 import numpy as np
 
 class AbstractGraph(ABC):
-    def __init__(self, dv, dc, N) -> None:
+    def __init__(self, dv, dc, N, max_i) -> None:
         self.dv = dv
         self.dc = dc
         self.N = N
         assert dv*N% dc == 0, "dc must dived N*dv"
         self.M = dv*N//dc
-        self.max_i = N
+        self.max_i = max_i
         self.c_node_connections = None
         self.v_node_connections = None
 
@@ -23,34 +22,73 @@ class AbstractGraph(ABC):
                   parity_check_mtx[i][node] = 1
         return parity_check_mtx
 
-    def decode(self, code):
-        assert len(code) == self.N, f"Message should be of length N = {self.N}."
+    def decode(self, r_vec):
+        def sum_v_node(v_node):
+            c_connection = self.v_node_connections[v_node]
+            return r_vec[v_node] + sum(
+                edge_cert[f"({v_node}, {c_node})"] for c_node in c_connection
+            )
+
+        assert len(r_vec) == self.N, f"Message should be of length N = {self.N}."
+        edge_cert = dict() # key: "(v_node, c_node)"
+        for v_node in range(self.N):
+            for c_node in self.v_node_connections[v_node]:
+                edge_cert[f"({v_node}, {c_node})"] = r_vec[v_node]
         for _ in range(self.max_i):
             # Reset iteration state
             has_error = False
-            num_failed_checks = self.N*[0]
-            # Check connections
-            for connection in self.c_node_connections:
-                check = reduce(lambda acc, node: acc ^ bool(code[node]), connection, False)
-                # Check failed
-                if check:
+            # Check C-Nodes
+            for c_node, v_connection in enumerate(self.c_node_connections):
+                # Find sign of c node
+                sign = reduce(
+                    lambda val, x: val*np.sign(x),
+                    [edge_cert[f"({v_node}, {c_node})"] for v_node in v_connection],
+                    1
+                )
+                if sign != 1:
                     has_error = True
-                    for node in connection:
-                        num_failed_checks[node] += 1
+                # Find minumum absolut values of c node
+                min_val = np.inf
+                second_min_val = np.inf
+                has_two_min = False
+                for v_node in v_connection:
+                    edge_val = abs(edge_cert[f"({v_node}, {c_node})"])
+                    if edge_val == min_val:
+                        second_min_val = min_val
+                        has_two_min = True
+                    elif edge_val < min_val:
+                        second_min_val = min_val
+                        min_val = edge_val
+                        has_two_min = False
+                    elif edge_val < second_min_val:
+                        second_min_val = edge_val
+                        has_two_min = False
+                # Calculate sign and absolute value of each edge
+                for v_node in v_connection:
+                    edge_sign = sign*np.sign(edge_cert[f"({v_node}, {c_node})"])
+                    old_edge_val = abs(edge_cert[f"({v_node}, {c_node})"])
+                    edge_val = (
+                        min_val
+                        if has_two_min or old_edge_val != min_val
+                        else second_min_val
+                    )
+                    edge_cert[f"({v_node}, {c_node})"] = edge_sign*edge_val
             # Code is fixed
             if not has_error:
                 break
-            # Sort nodes by most inequalities parity checks
-            num_failed_checks_per_node = sorted(
-                enumerate(num_failed_checks), key=lambda x: x[1], reverse=True
-            )
-            # Flip all bits that have the most number of inequalities
-            max_ineq = num_failed_checks_per_node[0][1]
-            for node, num_ineq in num_failed_checks_per_node:
-                if num_ineq < max_ineq:
-                    break
-                code[node] = int(not code[node])
-        return code
+            # Change message transmitted by V-Nodes
+            for v_node, c_connection in enumerate(self.v_node_connections):
+                node_sum = sum_v_node(v_node)
+                for c_node in c_connection:
+                    edge_cert[f"({v_node}, {c_node})"] = (
+                        node_sum - edge_cert[f"({v_node}, {c_node})"]
+                    )
+        # Decode message
+        msg = np.array(
+            [1 if sum_v_node(v_node) > 0 else 0 for v_node in range(self.N)],
+            dtype=np.int8
+        )
+        return msg
     
     def export_graph(self, path):
         v_node_connections = [
@@ -63,8 +101,8 @@ class AbstractGraph(ABC):
 
 
 class RandomGraph(AbstractGraph):
-    def __init__(self, dv, dc, N) -> None:
-        super().__init__(dv, dc, N)
+    def __init__(self, dv, dc, N, max_i) -> None:
+        super().__init__(dv, dc, N, max_i)
         random.seed(10)
         while True:
             error= False
