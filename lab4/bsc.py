2241b342818fdeb92516c94ee6fe2b4bce273e5e
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2023-07-12 00:42:44 +0200

    fix decoding

--- lab4/bsc.py
+++ lab4/bsc.py
@@ -15,6 +15,14 @@ def get_bitflip_prob(info_energy_noise_ratio, rate):
       return 0.5*erfc(2*sqrt(info_energy_noise_ratio*rate))     
 
 
+def bits2signal(bits):
+      return np.fromiter(map(lambda bit: -1 if bit == 1 else 1, bits), dtype=float)
+
+
 def gauss_channel_signal(signal, n0):
       variance = 0.5*n0
       return signal + np.random.normal(0, sqrt(variance), np.size(signal))
+
+
+def bits2noisy_signal(bits, n0):
+      return gauss_channel_signal(bits2signal(bits), n0)
