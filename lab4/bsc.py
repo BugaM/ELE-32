649851fae5589285900800fb66203cad1a7f8862
buga <marcelo.buga@hotmail.com>
2023-07-11 19:54:07 -0300

    adding new plot

--- lab4/bsc.py
+++ lab4/bsc.py
@@ -1,5 +1,5 @@
 from math import erfc, sqrt
-
+from scipy.stats import norm
 import numpy as np
 
 def bsc(array, p):
@@ -12,9 +12,17 @@ def bsc(array, p):
 
 
 def get_bitflip_prob(info_energy_noise_ratio, rate):
-      return 0.5*erfc(2*sqrt(info_energy_noise_ratio*rate))     
+      return 1 - norm.cdf(sqrt(2*info_energy_noise_ratio*rate))
 
 
 def gauss_channel_signal(signal, n0):
       variance = 0.5*n0
       return signal + np.random.normal(0, sqrt(variance), np.size(signal))
+
+def prob_to_ssr(prob, rate):
+      def calculate_inverse_q_function(p):
+            return norm.ppf(1 - p)
+      inv_q = calculate_inverse_q_function(prob)
+      eb_n0 = 0.5* inv_q**(2)
+      return eb_n0/rate
+
--- lab4/probability_curves.py
+++ lab4/probability_curves.py
@@ -1,48 +1,75 @@
-from bsc import bsc
+from bsc import prob_to_ssr
 import numpy as np
 import pickle
+from math import log10
 
 import matplotlib.pyplot as plt
-import matplotlib
 
-# PE_CYCLING = [0.500749906261717, 0.278515185601800, 0.0912738590767615,
-#                0.0197060036749541, 0.00166247921900976, 0.000254999681250398, 
-#                2.99999625000469e-05, 4.99999375000781e-06, 6.24999921875010e-07, 
-#                1.87499976562503e-07, 0, 0]
-HAMMING_K = 4
-P = [0.05,0.02,0.01]
+
+P = [0.5,0.2,0.1]
 
 
 def get_probabilities(P):
+      p = []
+      while P[-1] > 10**-5:
+            p += P
+            P = [x/10 for x in P]
+      return p
+P_BF = [0.05,0.02,0.01]
+
+
+def get_bitflipping_probs(P):
       p = [0.1]
       while P[-1] >= 10**-5:
             p += P
             P = [x/10 for x in P]
       return p
+def db(v):
+      if (v > 0):
+            return 10*log10(v)
+      else: 
+            return None
 
 probabilities = get_probabilities(P)
 
+ssr_hamming = [db(prob_to_ssr(p, (4/7))) for p in probabilities]
 with open("hamming.pkl", "rb") as fp:   #Pickling
       pe_hamming = pickle.load(fp)
+ssr_no_encoding = [db(prob_to_ssr(p,1)) for p in probabilities]
 with open("no_encoding.pkl", "rb") as fp:   #Pickling
       pe_no_encoding = pickle.load(fp)
-with open("graph.pkl", "rb") as fp:
-      pe_graphs = pickle.load(fp)
+
+
+pe_cycling = [0.500749906261717, 0.278515185601800, 0.0912738590767615,
+               0.0197060036749541, 0.00166247921900976, 0.000254999681250398, 
+               2.99999625000469e-05, 4.99999375000781e-06, 6.24999921875010e-07, 
+               1.87499976562503e-07, 0, 0]
+
+ssr_cycling = [db(prob_to_ssr(p, (9/17))) for p in probabilities]
+
+
+bf_probabilities = get_bitflipping_probs(P_BF)
+
+with open("bitflipping.pkl", "rb") as fp:
+      pe_bitflipping_list = pickle.load(fp)
+      pe_bitflipping = pe_bitflipping_list[3]
+ssr_bf = [db(prob_to_ssr(p, (4/7))) for p in bf_probabilities]
+
 
 plt.ymax = 0.5
 ax = plt.gca()
 plt.yscale("log")
-plt.xscale("log")
-ax.set_xlim(0.1, probabilities[-1])
-plt.xlabel("p")
+# plt.xscale("log")
+plt.xlabel("Ei/N0 (db)")
 plt.ylabel("Pe")
+plt.xticks([0,0.5,1,1.5,2,2.5,3,3.5,4,4.5,5])
+ax.set_xlim(0,5)
 
-plt.plot(probabilities, pe_hamming[2:], label="Hamming")
-plt.plot(probabilities, pe_no_encoding[2:], label="No encoding")
-plt.plot(probabilities, pe_graphs[0], label="100")
-plt.plot(probabilities, pe_graphs[1], label="200")
-plt.plot(probabilities, pe_graphs[2], label="500")
-plt.plot(probabilities, pe_graphs[3], label="1000")
+plt.plot(ssr_hamming, pe_hamming[:len(ssr_hamming)], label="Hamming")
+plt.plot(ssr_no_encoding, pe_no_encoding[:len(ssr_no_encoding)], label="No encoding")
+plt.plot(ssr_cycling, pe_cycling[:len(ssr_cycling)], label="Cycling")
+plt.plot(ssr_bf, pe_bitflipping, label="Bit flipping N=994")
 plt.legend()
+plt.grid()
 
 plt.show()
